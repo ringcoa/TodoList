aafdbb54ccd68ecf21de06c351b2639f7e7e39ea
ringcoa <gkakdlv@gmail.com>
2020-08-16 11:42:01 +0900

    Updates

---
+++ precache-manifest.4b4db54aab5a9811cc91162eaa5fa0c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4eaa70f7bfc4e03f22b5d0bc5a9b297",
+    "url": "/ringcoa/TodoList/index.html"
+  },
+  {
+    "revision": "bed03ebca444ab93b882",
+    "url": "/ringcoa/TodoList/static/css/main.81e92114.chunk.css"
+  },
+  {
+    "revision": "f0281200200c4dffc71d",
+    "url": "/ringcoa/TodoList/static/js/2.601c4d52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ringcoa/TodoList/static/js/2.601c4d52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bed03ebca444ab93b882",
+    "url": "/ringcoa/TodoList/static/js/main.024f8525.chunk.js"
+  },
+  {
+    "revision": "e4d1c1e969d52006be9c",
+    "url": "/ringcoa/TodoList/static/js/runtime-main.7ec44099.js"
+  }
+]);
